dbab263c0a00c57c96e7fa87cf8bcc4e66ec05e2
shirleycelis <shirleyguadalupecd@ufps.edu.co>
2024-04-04 22:14:13 -0500

    chore:adding linters to makefile

--- make.bat
+++ make.bat
@@ -12,3 +12,14 @@ if "%1"=="migrate" (
     python manage.py migrate
 )
 
+rem Command to lint a specific file with flake8
+if "%1"=="lint" (
+    call .\venv\Scripts\activate.bat
+    flake8 %2
+    black %2
+)
+
+rem Command Activate venv
+if "%1"=="activate" (
+    call .\venv\Scripts\activate.bat
+)
